da6acc2aa903079548329bf990b87dec477bb049
Rim Vilgalys <vilgalys@gmail.com>
2025-01-26 12:59:22 -0500

    uddate page title

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 const Layout = ({ children, title }) => (
   <>
     <Head>
-      <title>{"🦖 Rim.Works 🦕" + (title ? " | " + title : "")}</title>
+      <title>{"🦖 rim.computer 🦕" + (title ? " | " + title : "")}</title>
       <link rel="stylesheet" href="https://use.typekit.net/zdk0lrz.css"></link>
     </Head>
     {children}
