b6e9fc060135f913e5f28b067766a58b46445050
Rim Vilgalys <vilgalys@gmail.com>
2025-02-04 08:58:30 -0500

    fix page routing for google analytics third party lib

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "../styles/styles.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-KZHRMR7BVH"></GoogleAnalytics>
+    </>
+  );
 }
